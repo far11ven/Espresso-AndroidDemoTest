bb05d2205ae89a33b2d710575ccc8d823e548224
GRYPHON\far11ven <kushalbhalaik@gmail.com>
2018-09-05 18:49:16 +0530

    #24

--- app/src/androidTest/java/com/mytaxi/android_demo/LoginTestTaskPositiveScenario.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/LoginTestTaskPositiveScenario.java
@@ -1,8 +1,6 @@
 package com.mytaxi.android_demo;
 
 
-import android.support.test.espresso.IdlingRegistry;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
@@ -26,20 +24,17 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
-import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
 public class LoginTestTaskPositiveScenario {
 
-    private IdlingResource mainActivityIdlingResource;
-	private IdlingResource loginBtnIdlingResource;
+
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -52,16 +47,6 @@ public class LoginTestTaskPositiveScenario {
             android.Manifest.permission.ACCESS_FINE_LOCATION);                  // granting permission for user map location access during test
 
 
-    /* This is a setUp method() before running a test
-       - Sets up IdlingResources before each test
-    */
-    @Before
-    public void setUpBeforeTest() {
-        loginBtnIdlingResource = new LoginButtonIdlingResource();
-        mainActivityIdlingResource = new MainActivityIdlingResource();
-        Intents.init();                                                 //initialising intents
-
-    }
 
     /* This is a Test method for TASK#1 [Positive scenario]
        - Logs in with the valid credentials
@@ -87,17 +72,10 @@ public class LoginTestTaskPositiveScenario {
         // to check if LOGIN button is displayed and can be clicked
         onView(withId(R.id.btn_login)).check(matches(isDisplayed())).check(matches(isClickable()));
         onView(withId(R.id.btn_login)).perform(click()) ; // click LOGIN to submit
-		
-		IdlingRegistry.getInstance().register(loginBtnIdlingResource);
 
-        IdlingRegistry.getInstance().register(mainActivityIdlingResource);
-		
-		IdlingRegistry.getInstance().unregister(loginBtnIdlingResource);
 
         onView(withId(R.id.textSearch))
                 .check(matches(isDisplayed()));                               //checks if driver serach field is displayed on view
-
-        IdlingRegistry.getInstance().unregister(mainActivityIdlingResource);  // ending wait for MainActivity
 
         onView(withId(R.id.drawer_layout))
                 .check(matches(isClosed(Gravity.LEFT))) // To check Left Drawer based on that it is currently closed.
@@ -111,16 +89,6 @@ public class LoginTestTaskPositiveScenario {
                 .perform(DrawerActions.close());       // closes the left drawer
 
 
-    }
-
-    /* This is a @After method
-      - Releases up resources, un-registering IdlingResources if remaining
-    */
-    @After
-    public void tearDownAfterTest() {
-
-        IdlingRegistry.getInstance().unregister(mainActivityIdlingResource);
-        Intents.release();
     }
 
 }
